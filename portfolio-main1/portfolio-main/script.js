fdebef3f92a4689b059895981cdda137b35d690c
yaduvamsi0810 <161410741+yaduvamsi0810@users.noreply.github.com>
2024-07-25 21:43:38 +0530

    Add files via upload

---
+++ portfolio-main1/portfolio-main/script.js
@@ -0,0 +1,10 @@
+function fet() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+    console
+}
